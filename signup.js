3605d1a4889957c0e0cf2d84c2a166cfda456c0c
parvtaneja <109197360+parvtaneja@users.noreply.github.com>
2023-05-26 16:27:47 +0530

    New Files

---
+++ signup.js
@@ -0,0 +1,10 @@
+   document.getElementById("submit").onclick = function jsfunc(){
+         Moengage.add_unique_user_id(document.getElementById("fname").value); 
+         
+         Moengage.add_last_name(document.getElementById("lname").value);
+         Moengage.add_gender(document.getElementById("gender").value);
+         Moengage.add_email(document.getElementById("email").value);
+      console.log("parv taneja");
+      
+
+      }
